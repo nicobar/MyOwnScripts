f28a112e8407b5e4b971299f31cf25ab1c70e085
Andrea Spera <andrea.spera@gmail.com>
2018-10-15 19:35:27 +0200

    added text_analisys class

--- MyOwnScript/get_command_ssh2_oop.py
+++ MyOwnScript/get_command_ssh2_oop.py
@@ -1,28 +1,30 @@
 import pexpect
 import json
+from os import listdir
+from os.path import isfile, join
 
 class remote_cmd:
     def __init__(self, conn_filename):
         
         self.conn_filename = conn_filename
-        self.data_conn = self.__get_conn_data()
+        self.json_data = self.__get_json_data()
         self.node_name_list = self.__get_node_name_list()
 
     def populate_dir_with_show_command(self):
  
-        cmd_dict = {cmd: "_".join(cmd.split(" ")) for cmd in self.data_conn[0]["cmd_list"]}
+        cmd_dict = {cmd: "_".join(cmd.split(" ")) for cmd in self.json_data[0]["cmd_list"]}
         
         for node in self.node_name_list:
             if node is not "":
                 for command in cmd_dict:
                     self.__get_remote_cmd(node, command, cmd_dict[command])  
 
-    def __get_conn_data(self):
+    def __get_json_data(self):
         with open (self.conn_filename) as f:
             return json.load(f)
         
     def __get_node_name_list(self):
-        with open(self.data_conn[0]["base_dir"] + self.data_conn[0]["device_names_file"], 'r') as fin:
+        with open(self.json_data[0]["base_dir"] + self.json_data[0]["device_names_file"], 'r') as fin:
             l_s = fin.readlines()
         return  [ s.strip("\r\n") for s in l_s]       
     
@@ -30,7 +32,7 @@ class remote_cmd:
         ''' This function read devices names from file,
          connects to them and write on file output of a file '''
     
-        cmd_ssh_bridge = 'ssh -y ' + self.data_conn[0]["myusername"] + '@' + self.data_conn[0]["bridge_name"]  
+        cmd_ssh_bridge = 'ssh -y ' + self.json_data[0]["myusername"] + '@' + self.json_data[0]["bridge_name"]  
         cmd_telnet_node = 'telnet ' + node_name
         file_name = node_name + '_' + fname + '.txt'
         string_to_expect = str.upper(node_name)  + '#'
@@ -38,44 +40,87 @@ class remote_cmd:
         child = pexpect.spawn(cmd_ssh_bridge, encoding='utf-8', codec_errors='replace')
     
         child.expect('Password: ')
-        child.sendline(self.data_conn[0]["mybridgepwd"])
+        child.sendline(self.json_data[0]["mybridgepwd"])
         child.expect('\$')
     
         child.sendline(cmd_telnet_node)
         child.expect('username: ')
-        child.sendline(self.data_conn[0]["myusername"])
+        child.sendline(self.json_data[0]["myusername"])
         child.expect('password: ')
-        child.sendline(self.data_conn[0]["mytacacspwd"])
+        child.sendline(self.json_data[0]["mytacacspwd"])
         child.expect(string_to_expect)
         child.sendline('term len 0')
         child.expect(string_to_expect)
     
         child.sendline(cmd)
     
-        with open(self.data_conn[0]["base_dir"] + file_name, 'w') as fout:
+        with open(self.json_data[0]["base_dir"] + file_name, 'w') as fout:
             child.logfile_read = fout
             child.expect(string_to_expect)
     
         child.terminate()
 
+class text_analisys:
+    
+    def __init__(self, conn_filename):
+        self.conn_filename = conn_filename
+        self.json_data = self.__get_json_data()
+        #self.json_data[0]["base_dir"]
+        
+    def __get_json_data(self):
+        with open (self.conn_filename) as f:
+            return json.load(f)
+        
+    def create_output(self):
+        ''' from <switches_>_command files creates output_commnad file
+        with "<switch> command output" as lines '''
+
+        file_list = [f for f in listdir(self.json_data[0]["base_dir"]) if isfile(join(self.json_data[0]["base_dir"], f)) and "VPE" in f]
+        for file in file_list:
+            #input_file_name = base_dir + file
+            list_f = file.split('_')
+            node = list_f[0]
+    
+            #cmd_list = file_list[2:-1] + [file_list[-1:][0][:-4]]
+            #cmd_string = '_'.join(cmd_list)
+            #output_file_name = base_dir + 'output_' + cmd_string + '.txt'
+            with open(self.json_data[0]["base_dir"] + file, 'r') as fin:
+                input_text_list = fin.readlines()
+            with open(self.json_data[0]["base_dir"] + self.json_data[0]["output_file_name"], 'a') as fout:              
+                for line in input_text_list[3:-1]:
+                    if len(input_text_list) >3:
+                        stripped_line = line.strip()
+                        #line_list = stripped_line.split()
+                        rd = stripped_line.split(';')[1][4:]
+                        right_line = node + ' ' + rd + '\n'                 
+                    else:
+                        right_line = node + ' ' + 'None\n'
+                    fout.write(right_line)
+        print('end writing')
 
 
 
 #### MAIN ###
 
-# cat /mnt/hgfs/VM_shared/MyOwnScripts/getcommand/conn_data.txt
+# cat /mnt/hgfs/VM_shared/MyOwnScripts/get_rd/conn_data.txt
 # [{
 #   "bridge_name": "10.192.10.8",
 #   "myusername": "zzasp70",
 #   "mybridgepwd": "S!Pr0094",
-#   "mytacacspwd": "!SPra0094",
-#   "base_dir": "/mnt/hgfs/VM_shared/MyOwnScripts/getcommand/", 
-#   "device_names_file": "devices_XR_unique_sorted.txt",
+#   "mytacacspwd": "SPra0!094",
+#   "base_dir": "/mnt/hgfs/VM_shared/MyOwnScripts/get_rd/", 
+#   "device_names_file": "show_isis_db_uniq_vpeonly.txt",
+#   "output_file_name": "output_rd.txt",
+#   "comment1": "show bgp vrf OPNET process | i Distinguisher",
 #   "cmd_list":  [
-#                 "show rpl route-policy",
-#                 "show rpl community-set"
-#                 ]
+#                 "show vrf OPNET detail | i RD"
+#                ]
 # }]
 
-create_show_cmd_files = remote_cmd('/mnt/hgfs/VM_shared/MyOwnScripts/getcommand/conn_data.txt')
-create_show_cmd_files.populate_dir_with_show_command()
+if __name__ == "__main__":
+    
+    json_file = '/mnt/hgfs/VM_shared/MyOwnScripts/get_rd/conn_data.txt'
+#    create_show_cmd_files = remote_cmd(json_file)
+#    create_show_cmd_files.populate_dir_with_show_command()
+    testo = text_analisys(json_file)
+    testo.create_output()
